44096e7842bd02a9f1dc7d144070720066d350a9
Vincent REHM <vincentrehm@yahoo.fr>
2020-09-19 17:52:47 +0200

    nuxt-crate-app + add tailwind ui with some newsletter component for testing purpose

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/ui')],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
